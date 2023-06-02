e0f858033129061c4160b1d2128fb2d96fac761a
Brian Moya <122998981+brianmoyar89@users.noreply.github.com>
2023-06-02 23:41:53 +0000

    Generador de Dominios Final

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
+var pronoun = ["the", "our"];
+var pronoun = ["the", "our"];
+var adj = ["great", "big"];
+var noun = ["jogger", "racoon"];
+var extensions = [".com", ".net", ".us", ".io", ".es"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+var generatorButton = document.getElementById("domain-generator");
+var domainContainer = document.getElementById("domain-container");
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+generatorButton.addEventListener("click", generateDomain);
+
+function generateDomain() {
+  var randomPronoun = pronoun[Math.floor(Math.random() * pronoun.length)];
+  var randomAdj = adj[Math.floor(Math.random() * adj.length)];
+  var randomNoun = noun[Math.floor(Math.random() * noun.length)];
+  var randomExtension =
+    extensions[Math.floor(Math.random() * extensions.length)];
+
+  var domain = randomPronoun + randomAdj + randomNoun + randomExtension;
+
+  var domainElement = document.createElement("div");
+  domainElement.textContent = domain;
+
+  domainContainer.innerHTML = "";
+  domainContainer.appendChild(domainElement);
+}
